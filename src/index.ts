2c81506b88064dccbfc2dbfac424597c2736ec52
Julien Goudard <juliengoudard.g@gmail.com>
2023-09-19 14:00:49 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express, { Express, Request, Response } from "express";
+import { Ad } from "./utils/types";
+const app: Express = express();
+app.use(express.json());
+const port: number = 3000;
+
+
+
+const ads: Ad[] = [
+  {
+    id: 1,
+    title: "Bike to sell",
+    description:
+      "My bike is blue, working fine. I'm selling it because I've got a new one",
+    owner: "bike.seller@gmail.com",
+    price: 100,
+    picture:
+      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+    location: "Paris",
+    createdAt: "2023-09-05T10:13:14.755Z",
+  },
+  {
+    id: 2,
+    title: "Car to sell",
+    description:
+      "My car is blue, working fine. I'm selling it because I've got a new one",
+    owner: "car.seller@gmail.com",
+    price: 10000,
+    picture:
+      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+    location: "Paris",
+    createdAt: "2023-10-05T10:14:15.922Z",
+  },
+];
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/ad", (req: Request, res: Response) => {
+  res.send(ads);
+});
+
+app.post("/ad", (req: Request, res: Response) => {
+  const newId: number = ads.length + 1;
+  ads.push({ ...req.body,
+     id: newId,
+     createdAt: new Date().toISOString() });
+  res.send("Request received, check the backend terminal");
+});
